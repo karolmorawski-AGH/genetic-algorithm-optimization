d1b69564e42f726f08e95171a61dbc734ea7fdf7
Karol Morawski <karol.morawski4@gmail.com>
2019-12-11 23:36:16 +0100

    some changes

--- optimize.py
+++ optimize.py
@@ -1,25 +1,59 @@
 from gaft import GAEngine
 from gaft.components import BinaryIndividual
 from gaft.components import Population
-from gaft.operators import TournamentSelection
+from gaft.operators import TournamentSelection, ExponentialRankingSelection, LinearRankingSelection, RouletteWheelSelection
 from gaft.operators import UniformCrossover
 from gaft.operators import FlipBitBigMutation
 
-import math
+# Rosenbrock function
+# Expected solution: [1,1] = 0
+
+# Function boundaries
+XB = (-10, 10)
+YB = (-10, 10)
+# Decrete precision of binary sequence
+EPS = 0.0001
+
+# Population size
+POPULATION_SIZE = 250
+
+# Selection method
+SELECTION = RouletteWheelSelection()
+
+# Crossover method
+# pc: probability of crossover (0,1)
+PC = 0.8
+# pe: probability of gene exchange (0,1)
+PE = 0.5
+
+# Mutation
+# pm: probability of mutation (0, 1)
+PM = 0.05
+# pbm: probability of big mutation (5 times bigger than pbm)
+PBM = PM * 5
+# alpha: intensive factor (0.5, 1)
+ALPHA = 0.6
+
+# Run
+# ng: Evolution iteration steps (generations number)
+NG = 150
+
 
 # Built-in best fitness analysis.
+
 from gaft.analysis.fitness_store import FitnessStore
 from gaft.analysis.console_output import ConsoleOutput
 
 # Define population.
-indv_template = BinaryIndividual(ranges=[(-2, 2), (-2, 2)], eps=0.001)
-population = Population(indv_template=indv_template, size=50).init()
+
+
+indv_template = BinaryIndividual(ranges=[XB, YB], eps=EPS)
+population = Population(indv_template=indv_template, size=POPULATION_SIZE).init()
 
 # Create genetic operators.
-#selection = RouletteWheelSelection()
-selection = TournamentSelection()
-crossover = UniformCrossover(pc=0.8, pe=0.5)
-mutation = FlipBitBigMutation(pm=0.1, pbm=0.55, alpha=0.6)
+selection = SELECTION
+crossover = UniformCrossover(pc=PC, pe=PE)
+mutation = FlipBitBigMutation(pm=PM, pbm=PBM, alpha=ALPHA)
 
 # Create genetic algorithm engine.
 # Here we pass all built-in analysis to engine constructor.
@@ -33,6 +67,7 @@ engine = GAEngine(population=population, selection=selection,
 @engine.minimize
 def fitness(indv):
     x, y = indv.solution
-    return (1-x)* (1-x) + 100 * (y-x*x) * (y-x*x)
+    return (1 - x) * (1 - x) + 100 * (y - x * x) * (y - x * x)
 
-engine.run(ng=150)
+
+engine.run(ng=NG)
